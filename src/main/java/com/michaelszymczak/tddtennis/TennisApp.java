9cbaed65aa8a70251727e4d0ebec493ac80c6a80
Michael Szymczak <contact@michaelszymczak.com>
2017-05-20 21:07:48 +0100

    [tdd-tennis] Start of the game

--- src/main/java/com/michaelszymczak/tddtennis/TennisApp.java
+++ src/main/java/com/michaelszymczak/tddtennis/TennisApp.java
@@ -1,9 +1,17 @@
 package com.michaelszymczak.tddtennis;
 
 public class TennisApp {
+
   public static void main(String[] args) {
     System.out.println("Hello TDD Tennis.");
   }
 
+  public static TennisApp startGame() {
+    return new TennisApp();
+  }
+
+  public String result() {
+    return "love";
+  }
 
 }
--- src/test/groovy/com/michaelszymczak/tddtennis/TennisAppTest.groovy
+++ src/test/groovy/com/michaelszymczak/tddtennis/TennisAppTest.groovy
@@ -2,9 +2,12 @@ package com.michaelszymczak.tddtennis
 
 import spock.lang.Specification
 
+import static com.michaelszymczak.tddtennis.TennisApp.startGame
+
 class TennisAppTest extends Specification {
 
-  def "foo"() {
-    expect: true
+  def "no player should have any points upo starting the game"() {
+    expect:
+    startGame().result() == "love"
   }
 }
